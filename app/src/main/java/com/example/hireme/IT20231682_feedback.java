7cb81b9f8d5e75d87398c841e9bc67037438f3a8
kushaniSakalasooriya <79516521+kushaniSakalasooriya@users.noreply.github.com>
2021-08-11 21:00:54 +0530

    created form

---
+++ app/src/main/java/com/example/hireme/IT20231682_feedback.java
@@ -0,0 +1,13 @@
+package com.example.hireme;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class IT20231682_feedback extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_it20231682_feedback);
+    }
+}
