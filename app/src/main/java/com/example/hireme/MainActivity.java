1199b68ad855cfde07d0ea37f561d2a54ae1dd6f
Isuru Sahan <63364509+echoOIsuru@users.noreply.github.com>
2021-08-11 20:19:11 +0530

    added file types and modify menu

--- app/src/main/java/com/example/hireme/MainActivity.java
+++ app/src/main/java/com/example/hireme/MainActivity.java
@@ -26,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        //getSupportActionBar().hide();
+        getSupportActionBar().hide();
 
         //Splash animation
         topAnim = AnimationUtils.loadAnimation(this,R.anim.top_animation);
